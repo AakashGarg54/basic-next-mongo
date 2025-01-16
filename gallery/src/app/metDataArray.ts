e4c9b90b6efb0002e5a190a90314690cf2eb0207
aakashgarg54 <aakashgarg5518@gmail.com>
2025-01-16 12:29:42 +0530

    Adding gallery-app

---
+++ gallery/src/app/metDataArray.ts
@@ -0,0 +1,11 @@
+import { StaticImageData } from "next/image";
+import photo1 from "@/app/photos/photo (1).jpg"
+import photo2 from "@/app/photos/photo (2).jpg"
+import photo3 from "@/app/photos/photo (3).jpg"
+import photo4 from "@/app/photos/photo (4).jpg"
+import photo5 from "@/app/photos/photo (5).jpg"
+import photo6 from "@/app/photos/photo (6).jpg"
+import photo7 from "@/app/photos/photo (7).jpg"
+import photo8 from "@/app/photos/photo (8).jpg"
+
+
